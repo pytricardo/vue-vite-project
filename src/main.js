d7f8748075fc99df3471450b73d0a5518a1be766
pytricardo <rfrailen@yahoo.es>
2024-03-04 14:11:30 +0000

    primer commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "bootstrap"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./assets/scss/styles.scss"
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
