601399d6881e1657507575b9270baa0963de123b
Ricardo FN <rfrailen@yahoo.es>
2024-03-08 10:55:55 +0000

    Añado opción contacto

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contacto">Contacto</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/ContactoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="contacto">
+    <h1>This is an contacto page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
